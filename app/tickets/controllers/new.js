861d114a48ef0627927a7daf115058ececefc94e
Aurelien Roy <roy.aurelien@gmail.com>
2016-12-01 19:22:20 +0100

    formulaire nouveau ticket

--- app/tickets/controllers/new.js
+++ app/tickets/controllers/new.js
@@ -1,4 +1,31 @@
 
 myapp.controller('NewTicketCtrl', function($scope, $rootScope, TicketsService){
     $rootScope.page.title = 'Nouveau ticket';
+
+    $scope.readonly = false;
+    $scope.removable = false;
+
+    // Lists of fruit names and Vegetable objects
+    $scope.fruitNames = ['MG5650'];
+
+  $scope.brands = [
+      "canon", 
+      "brother", 
+      "hp", 
+      "epson", 
+      "samsung"
+  ];
+
+  $scope.sizes = [
+      "informatique"
+  ];
+  $scope.toppings = [
+    { category: 'imprimante et multifonction', name: 'imprimante thermique' },
+    { category: 'imprimante et multifonction', name: 'imprimante sublimation' },
+    { category: 'imprimante et multifonction', name: 'multifonction laser' },
+    { category: 'imprimante et multifonction', name: 'imprimante laser' },
+    { category: 'high tech', name: 'pc portable' },
+    { category: 'high tech', name: 'smartphone et tablette' }
+  ];
+
 });
